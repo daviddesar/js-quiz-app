43f821399ce29e1a210875feb410dd7d4a4b2c04
Quan Nguyen (Dave) <hoangquan.nguyen@employmenthero.com>
2023-01-14 18:37:04 +0700

    add pattern and format

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const { pathname, origin } = req.nextUrl;
-  if (pathname == "/") {
+  if (pathname == '/') {
     return NextResponse.redirect(`${origin}/questions`);
   }
   return NextResponse.next();
