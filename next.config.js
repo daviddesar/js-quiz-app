3e2f5bef44b34ba9badb0d2cc8d39bf6001ba696
Quan Nguyen (Dave) <hoangquan.nguyen@employmenthero.com>
2023-01-23 21:16:29 +0700

    fix cdn link

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/daviddesar/daviddesar.github.io/js-quiz-app//gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/daviddesar/daviddesar.github.io/js-quiz-app/gh-pages/' : '',
 }
